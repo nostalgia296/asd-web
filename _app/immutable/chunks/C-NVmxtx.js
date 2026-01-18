2a468c939dbd8787a27cc06f667ea2bdec293cc3
nostalgia296 <nostalgia296@users.noreply.github.com>
2026-01-18 09:47:50 +0000

    deploy: f00019009e31fb26c26222abd4b574de014363fc

---
+++ _app/immutable/chunks/C-NVmxtx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ohdiwn?.base??"/asd-web",a=globalThis.__sveltekit_ohdiwn?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/Dhli-JCt.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_2nr06w?.base??"/asd-web",a=globalThis.__sveltekit_2nr06w?.assets??s??"";export{a,s as b};
